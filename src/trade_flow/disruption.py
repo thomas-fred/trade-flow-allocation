2e98e2630d3e83f5ca0a0171750483c3f03c5bed
Fred Thomas <fred.thomas@eci.ox.ac.uk>
2024-07-24 20:51:55 +0100

    Create unique edge id to filter splits by

--- src/trade_flow/disruption.py
+++ src/trade_flow/disruption.py
@@ -26,10 +26,16 @@ def filter_edges_by_raster(
         Network without edges experiencing raster values in excess of threshold.
     """
 
+    # we will create a new edge_id column, fail if we would overwrite
+    assert "edge_id" not in edges.columns
+
     # split out edges without geometry as snail will not handle them gracefully
     print("Parition edges by existence of geometry...")
     no_geom_edges = edges.loc[edges.geometry.type.isin({None}), :].copy()
     with_geom_edges = edges.loc[~edges.geometry.type.isin({None}), :].copy()
+
+    # we need an id to select edges by
+    with_geom_edges["edge_id"] = range(len(with_geom_edges))
 
     print("Read raster transform...")
     grid = snail.intersection.GridDefinition.from_raster(raster_path)
@@ -52,10 +58,10 @@ def filter_edges_by_raster(
     splits_with_values = splits_with_indices.copy()
     splits_with_values["raster_value"] = values
 
-    print(f"Filter out edges with splits in excess of {failure_threshold}...")
+    print(f"Filter out edges with splits experiencing values in excess of {failure_threshold} threshold...")
     failed_splits_mask = splits_with_values.raster_value > failure_threshold
-    failed_edge_ids = set(splits_with_values[failed_splits_mask].id.unique())
-    surviving_edges_with_geom = edges.loc[~edges.id.isin(failed_edge_ids), :]
+    failed_edge_ids = set(splits_with_values[failed_splits_mask].edge_id.unique())
+    surviving_edges_with_geom = edges.loc[~edges.edge_id.isin(failed_edge_ids), :]
 
     print("Done filtering edges...")
-    return pd.concat([no_geom_edges, surviving_edges_with_geom]).sort_index()
+    return pd.concat([no_geom_edges, surviving_edges_with_geom]).sort_index().drop(columns=["edge_id"])
